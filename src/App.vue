69a84d788cf71c4a8bc053f7ef222dde128052d3
roberto-palanga <r.palanga94@gmail.com>
2024-02-28 17:59:05 +0100

    creazione esercizio

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@
 
 <template>
   <div id="container">
-    <h1>Ciao belli</h1>
+    <h1>Stranger Things</h1>
+    <p>Una serie davvero bella, guardatela.</p>
+    <img src="https://images.ctfassets.net/4cd45et68cgf/22eaxyrfqLTOmD0ZgFJDX0/6d7b8a0f4c3130fd87c9921cbd11d180/image5.jpg?w=1200" alt="" srcset="">
 
   </div>
 
